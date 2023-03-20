94e0c122d96fe6c247aa7aba1b95f2846787e213
Adharsh R <adharshrajeeev2000@gmail.com>
2023-03-20 21:50:15 +0530

    structured frontent folders and installed necessary modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/UserPages/login/LoginPage'
+import SignupPage from './pages/UserPages/register/SignupPage'
+import HomePage from './pages/UserPages/home/HomePage'
+import ProfilePage from './pages/UserPages/profile/ProfilePage'
+
 
 function App() {
   return (
     <div>
-      <h1>CODER HUB</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LoginPage />} />
+          <Route path='/signUp' element={<SignupPage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/profile' element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
