af1acc75b648794b42ec591ee1a04f6584dda35b
Adharsh R <adharshrajeeev2000@gmail.com>
2023-03-21 23:31:54 +0530

    skeloten added

--- client/src/pages/UserPages/home/HomePage.jsx
+++ client/src/pages/UserPages/home/HomePage.jsx
@@ -1,12 +1,22 @@
-
 import React from 'react'
-import Navbar from '../../../components/UserComponets/NavbarComp/Navbar'
+import SideBar from '../../../components/UserComponets/LeftPanel/SideBar';
+import RightBar from '../../../components/UserComponets/SidePanel/RightBar';
+import Feed from '../../../components/UserComponets/UserFeed/Feed';
+import Navbar from '../../../components/UserComponets/NavbarComp/Navbar';
+import { Box, Stack } from '@mui/material'
 
 function HomePage() {
   return (
- 
-      <Navbar/>
-    
+
+    <Box >
+     <Navbar/>
+      <Stack direction="row" spacing="2" justifyContent="space-between" >
+      <SideBar />
+      <Feed />
+      <RightBar />
+      </Stack>
+    </Box>
+
   );
 }
 
