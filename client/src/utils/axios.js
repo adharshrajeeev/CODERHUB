269c5747611d44c9a45e2dd4e8caa09b30bd39dc
Adharsh R <adharshrajeeev2000@gmail.com>
2023-04-30 00:16:03 +0530

    added axios interceptor

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -6,5 +6,17 @@ const instance=axios.create({
     baseURL:baseUserUrl
 })
 
+instance.interceptors.response.use((response)=>{
+      return response;
+    
+},
+(error) => {
+    console.log(error.response.data)
+    if(error?.response?.data?.userBlocked){
+        localStorage.removeItem('token')
+    }
+   
+  
+})
 
 export default instance
