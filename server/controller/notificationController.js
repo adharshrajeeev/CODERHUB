ede0f4f587393efef832f115d5ab183addb8b20b
Adharsh R <adharshrajeeev2000@gmail.com>
2023-05-06 22:38:54 +0530

    added notification controller for handling notification req

---
+++ server/controller/notificationController.js
@@ -0,0 +1,28 @@
+import { changeNotificationReadStatus, fetchAllNotifications } from "../repositories/notificationRepository.js"
+
+
+
+export const getAllNotifications = async(req,res)=>{
+    try{
+
+        const {userId}=req.params
+        const {data}=await fetchAllNotifications(userId)
+        res.status(200).json(data)
+    }catch(err){
+        res.status(500).json({ error: err.message })
+
+    }
+
+}
+
+
+export const readNotification =async(req,res)=>{
+    try{
+        const {notificationId}=req.params
+        const {data}=await changeNotificationReadStatus(notificationId);
+        res.status(200).json(data)
+    }catch(err){
+        console.log(err.message)
+        res.status(500).json({ error: err.message })
+    }
+}
