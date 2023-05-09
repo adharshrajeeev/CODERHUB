622c9def829b3e848fd0d07dadee730fde3bd316
Adharsh R <adharshrajeeev2000@gmail.com>
2023-05-09 11:11:22 +0530

    added token validation instance

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -15,9 +15,11 @@ instance.interceptors.response.use((response)=>{
     
     if(error?.response?.data?.userBlocked){
         localStorage.removeItem('token')
-    }else 
-    if(error.message === 'Network Error' && !error.response){
+    }else if(error.message === 'Network Error' && !error.response){
       return  toast.error("NETWORK ERROR - Make Sure Api is Running")
+    }else if(error?.response?.data.authfalse){
+        localStorage.removeItem('token');
+         window.location.href='/'
     }
     else{
         return Promise.reject(error)
