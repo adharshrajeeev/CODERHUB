35c22e9f74cd9b6e538c1515cacca03f09514450
Adharsh R <adharshrajeeev2000@gmail.com>
2023-05-09 16:46:43 +0530

    changed req

--- client/src/pages/admin/adminPosts/AdminPostList.jsx
+++ client/src/pages/admin/adminPosts/AdminPostList.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import '../adminUserList/userLists.scss'
 import AdminNavbar from '../../../components/admin/navbar/Navbar'
 import AdminSidebar from '../../../components/admin/sidebar/Sidebar'
 import PostList from '../../../components/admin/postTable/PostList'
-import '../adminUserList/userLists.scss'
-import SampleTable from '../../../components/admin/tables/SampleTable'
+
 
 function AdminPostList() {
   return (
@@ -16,7 +16,6 @@ function AdminPostList() {
       </div>
       <div style={{margin:"15px"}} >
         <PostList/>
-        {/* <SampleTable/> */}
       </div>
     </div>
   </div>
