5e4d9d4dc7c85f8bcb3aa54f7205b0db0a972d4b
Adharsh R <adharshrajeeev2000@gmail.com>
2023-05-09 13:56:54 +0530

    added fetch all users and post services

--- client/src/api/AdminServices.js
+++ client/src/api/AdminServices.js
@@ -1,10 +1,28 @@
-import { ADMINLOGIN } from "../utils/ConstUrls";
+import { ADMINLOGIN, GET_ALL_POSTS, GET_ALL_USERS } from "../utils/ConstUrls";
 import { adminInstance } from "../utils/axios";
 
 
 export const adminLogin = async(body)=>{
     try{
         const response=await adminInstance.post(ADMINLOGIN,body);
+        return response.data
+    }catch(err){
+        throw err
+    }
+}
+
+export const fetchAllUsers=async()=>{
+    try{
+        const response=await adminInstance.get(GET_ALL_USERS);
+        return response.data
+    }catch(err){
+        throw err
+    }
+}
+
+export const fetchAllPosts=async()=>{
+    try{
+        const response=await adminInstance.get(GET_ALL_POSTS)
         return response.data
     }catch(err){
         throw err
