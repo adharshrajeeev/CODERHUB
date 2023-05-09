299dfea7634a0ae378077a23d142f8620069e2a0
Adharsh R <adharshrajeeev2000@gmail.com>
2023-05-09 13:22:51 +0530

    added adminlogin service

--- client/src/api/AdminServices.js
+++ client/src/api/AdminServices.js
@@ -1,11 +1,12 @@
-// import axios from '../utils/axios';
-// import store from '../redux/store';
+import { ADMINLOGIN } from "../utils/ConstUrls";
+import { adminInstance } from "../utils/axios";
 
 
-
-
-// export const AdminLogin = ()=>{
-//     return new Promise((resolve,reject)=>{
-
-//     })
-// }
+export const adminLogin = async(body)=>{
+    try{
+        const response=await adminInstance.post(ADMINLOGIN,body);
+        return response.data
+    }catch(err){
+        throw err
+    }
+}
