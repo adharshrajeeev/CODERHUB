72db4162f08d653d3af8e9bdbe28d8451d737936
Adharsh R <adharshrajeeev2000@gmail.com>
2023-05-09 14:21:26 +0530

    added montwise post and user report api calls

--- client/src/api/AdminServices.js
+++ client/src/api/AdminServices.js
@@ -1,4 +1,4 @@
-import { ADMINLOGIN, GET_ALL_POSTS, GET_ALL_USERS } from "../utils/ConstUrls";
+import { ADMINLOGIN, GET_ALL_POSTS, GET_ALL_USERS, POST_MONTH_WISE_COUNT, USER_MONTH_WISE_GROWTH } from "../utils/ConstUrls";
 import { adminInstance } from "../utils/axios";
 
 
@@ -24,6 +24,24 @@ export const fetchAllPosts=async()=>{
     try{
         const response=await adminInstance.get(GET_ALL_POSTS)
         return response.data
+    }catch(err){
+        throw err
+    }
+}
+
+export const fetchMontWiseUserReport = async ()=>{
+    try{
+        const response=await adminInstance.get(USER_MONTH_WISE_GROWTH)
+        return response
+    }catch(err){
+        throw err
+    }
+}
+
+export const fetchMonthWisePostReport = async ()=>{
+    try{
+        const response=await adminInstance.get(POST_MONTH_WISE_COUNT)
+        return response
     }catch(err){
         throw err
     }
