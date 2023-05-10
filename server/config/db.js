f3a390b5fa7850dc98812f725e537a05dcd7438f
Adharsh R <adharshrajeeev2000@gmail.com>
2023-05-10 14:54:20 +0530

    added atlas db

--- server/config/db.js
+++ server/config/db.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose"
 const connectDb= async ()=>{
     const connection=await mongoose.connect(process.env.MONGOURL,{ useNewUrlParser: true, useUnifiedTopology: true  })
     .then(()=>{
-        console.log(`DATABASE CONNECTED AT ${process.env.MONGOURL}`)
+        console.log(`DATABASE CONNECTED `)
     })
     .catch((err)=>{
         console.log(err)
     })
 }
-
+ 
 export default  connectDb
