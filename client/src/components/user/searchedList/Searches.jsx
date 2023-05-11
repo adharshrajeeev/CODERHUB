6098fa314e4cd8d0b1ab81e45a0e16b30a22452c
Adharsh R <adharshrajeeev2000@gmail.com>
2023-05-11 17:13:21 +0530

    changed search ui

--- client/src/components/user/searchedList/Searches.jsx
+++ client/src/components/user/searchedList/Searches.jsx
@@ -1,10 +1,41 @@
 import React, { useState } from 'react'
 import TextField from '@mui/material/TextField';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import axios from '../../../utils/axios'
 import toast,{Toaster} from 'react-hot-toast'
 import { SEARCH_ALL_USERS } from '../../../utils/ConstUrls';
 import { useNavigate } from 'react-router-dom';
+
+
+
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    }, 
+  }));  
+
+
 
 function Searches() {
 
@@ -42,9 +73,12 @@ function Searches() {
 
   return (
    <>
-     <TextField value={userSearchName} onChange={handleSearchChange}  placeholder='Search Users' id="fullWidth" />
-     <SearchOutlinedIcon onClick={handleSearchSubmit}/>
-     <Toaster />
+      <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+              value={userSearchName} onChange={handleSearchChange}
+            />
+             <SearchOutlinedIcon onClick={handleSearchSubmit}/>
    </>
   )
 }
